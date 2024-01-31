340b36e7c6419b4cc7c88010e2fd87652181c47f
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-01-31 22:00:36 +0530

    small change

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ require("dotenv").config();
 
 const port = process.env.PORT || 8081;
 
+app.use(express.static(__dirname + "/dist/"));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 console.log("port", process.env.PORT);
 app.get("/", (req, res) => {
   res.status(200).json({
