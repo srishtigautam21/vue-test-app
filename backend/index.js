26f83b8a32c3b39eae710adfd163f29305f177ed
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-02-01 14:46:32 +0530

    small change in index.js

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,18 @@ require("dotenv").config();
 
 const port = process.env.PORT || 8081;
 
-app.use(express.static(__dirname + "/dist/"));
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
-});
-// app.get("/",function(req,res){
-//   res.sendFile(path.join(__dirname, "../stock-visualizer/dist/index.html")),
-//   function(err){
-//     if(err){
-//       res.status(500).send(err)
-//     }
-//   }
-// })
+// app.use(express.static(__dirname + "/dist/"));
+// app.get("*", (req, res) => {
+//   res.sendFile(__dirname + "/dist/index.html");
+// });
+app.get("/",function(req,res){
+  res.sendFile(path.join(__dirname, "../stock-visualizer/dist/index.html")),
+  function(err){
+    if(err){
+      res.status(500).send(err)
+    }
+  }
+})
 console.log("port", process.env.PORT);
 app.get("/", (req, res) => {
   res.status(200).json({
