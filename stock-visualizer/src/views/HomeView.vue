fb7941740bd6fb714462b87ea169e16e6766657f
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-02-01 10:45:45 +0530

    Teasting deployment on vercel

--- stock-visualizer/src/views/HomeView.vue
+++ stock-visualizer/src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<template>
+<script setup>
+const resp = ref("");
+const callApi = async () => {
+  const response = await fetch("https://vue-test-app-api.vercel.app/");
+  const data = response.json();
+  resp.value = data;
+};
+</script>
+
+<template setup>
   <div class="home">
-    <div>Hello</div>
+    <button @click="callApi">Hello</button>
+    <div>{{ resp.value }}</div>
   </div>
 </template>
-
-<script></script>
