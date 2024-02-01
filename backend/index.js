8bcb476498e0470a4d7913252bdea14fe0f03b8c
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-02-01 13:25:48 +0530

    Added root package.json file

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,14 @@ app.use(express.static(__dirname + "/dist/"));
 app.get("*", (req, res) => {
   res.sendFile(__dirname + "/dist/index.html");
 });
-
+// app.get("/",function(req,res){
+//   res.sendFile(path.join(__dirname, "../stock-visualizer/dist/index.html")),
+//   function(err){
+//     if(err){
+//       res.status(500).send(err)
+//     }
+//   }
+// })
 console.log("port", process.env.PORT);
 app.get("/", (req, res) => {
   res.status(200).json({
